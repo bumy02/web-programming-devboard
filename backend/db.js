819641ab0918336beba9866e1575949e37e1c576
bumy02 <bumy0208@gmail.com>
2026-06-08 10:59:06 +0900

    DB SSL 강제 적용

--- backend/db.js
+++ backend/db.js
@@ -9,9 +9,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-   ssl: process.env.DB_HOST?.includes('render.com')
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
