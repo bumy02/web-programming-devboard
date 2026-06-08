69bcdce516f7f95b77606de7401027e45d8fbdd1
bumy02 <bumy0208@gmail.com>
2026-06-08 10:16:51 +0900

    API baseURL 환경변수 설정

--- backend/db.js
+++ backend/db.js
@@ -9,6 +9,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+   ssl: process.env.DB_HOST?.includes('render.com')
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
